fad2cb3c71b1c241f1eaacbe41f25555bab63698
Sepece <sepece@gmail.com>
2024-05-05 20:29:01 +0200

    Tarea 6 terminada

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -58,28 +58,63 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
 
-    
+    /**
+     * @param nombre variable de tipo String que sirve para determinar el nombre
+     */
     private String nombre;
+    
+    /**
+     * @param cuenta variable de tipo String que sirve para determinar el número de cuenta
+     */
     private String cuenta;
+    
+    /**
+     * @param saldo variable de tipo double que sirve para determinar el saldo
+     */
     private double saldo;
+    
+      /**
+     * @param tipoInteres variable de tipo double que sirve para determinar el tipo de interés
+     */
     private double tipoInterés;
 
+    
+    /**
+     * Sirve para crear una cuenta con los datos "predeterminados"
+     */
     public CCuenta()
     {
     }
 
+    
+    /**
+     * Crea una cuenta con datos introducidos en las variables explicadas anteriormente
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+/**
+ * 
+ * @return devuelve el saldo 
+ */
     public double estado()
     {
         return getSaldo();
     }
 
+    
+    /**
+     * Se utiliza para ingresar una cantidad de dinero en la cuenta
+     * @param cantidad variable de tipo double 
+     * @throws Exception indica que no se puede ingresar una cantidad negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -87,6 +122,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    
+    /**
+     * Se utiliza para retirar una cantidad de dinero de la cuenta
+     * @param cantidad vairable de tipo double
+     * @throws Exception indica que no se puede retirar una cantidad negativa o que no hay suficiente saldo para retirar la cantidad indicada
+     */ 
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
